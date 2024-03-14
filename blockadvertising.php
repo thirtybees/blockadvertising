b759b0ec0981f6fc701b86a128b1dcb9c7ea2287
Petr Hučík <petr@getdatakick.com>
2024-03-14 08:52:03 +0100

    Remove unused dynamic property

--- blockadvertising.php
+++ blockadvertising.php
@@ -304,7 +304,6 @@ class BlockAdvertising extends Module
         $lang = new Language((int) Configuration::get('PS_LANG_DEFAULT'));
         $helper->default_form_language = $lang->id;
         $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
-        $this->fields_form = array();
         $helper->identifier = $this->identifier;
         $helper->submit_action = 'submitAdvConf';
         $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false).'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
